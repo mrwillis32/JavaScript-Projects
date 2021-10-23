a90f25f38e46709f602df8889a273e6705141ecc
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-10-23 16:11:06 -0400

    corrections

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,31 @@
-window.alert("Hello, World!"); //This will pop up an alert window
+window.alert("Hello, World!"); //This will pop up an alert window//
 
 
+    var x = ("Peanut butter and Jelly" + "<br>" + "<br>");
+    document.write(x);
+  
+  
+    document.write(800 * 456 + "<br>" + "<br>");
+    // the answer to 800*456 will display on the wepage//
+  
+  
+    var blues = "It ain't easy being green." + "<br>" + "<br>";
+    var blues = blues.fontcolor("purple");
+    document.write(blues);
+    // here the variable "blues" is equal to " it aint easy being green." In the second line var blues = blues.fontcolor("purple") is letting us know that the variable now also represents a color and that color is purple. therefore when the document.write() function is used not only will the stateent "It ain't easy being green." be printed out on screen but it will also print out in purple.//
+
+  
+    let myAge = 62;
+    document.write("I am " + myAge + " years old." + "<br>" + "<br>");
+    //I am 62 years old will be printed on screen. the let myAge variable is equal to 62. In the document.write function I combined the string "I am" + the let var of myAge + years old to reach my outcome.//
+
+   var str1 = " Sunday ";
+   var str2 = " is for football ";
+   var res = str1 + str2;
+    document.write(res);
+    
+    // I assign two variables "str1" and "str2" and concatenate them to form the third variable "res" which when ran spells out "Sunday is for football"//
+    
+  
 
 
