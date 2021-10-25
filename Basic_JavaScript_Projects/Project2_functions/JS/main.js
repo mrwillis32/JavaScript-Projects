f2d799eb1dab6af9e7a1a5a7061c2746fb2db01e
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-10-24 21:55:03 -0400

    id update

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,5 +1,7 @@
 function myFunction() {
     var sentence = "I am learning";
     sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML=sentence;
+    sentence += " Reading is fun for your mental!"
+    document.getElementById("Join").innerHTML=sentence;
 }
+// I assigned the var sentence to equal " I am learning." Then using the += operators i added the string a lot from this book! 
