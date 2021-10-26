5a37d0c9012c50c9317d87899fcf93cec72ccf97
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-10-25 21:54:03 -0400

    Update main.js

--- Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
 function my_Dictionary()   {
-    var Cars = {
+    var Cars = {            
         Model:"Altima",
         Color:"Black",
         Make:"Nissan",
-        Doors:4,
+        Doors:4,        // numbers don't need to be put in quotations 
         Fuel:"Gas"
     };
-    delete Vehicle.Fuel;
-    document.getElementById("Dictionary").innerHTML = Vehicle.Fuel;
+    delete Cars.Fuel; // Function deletes the fuel attribute from the code. The output will now show undefined. Instead of using an operator symbol we can also use words to get the same effect
+    document.getElementById("Dictionary").innerHTML = Cars.Fuel; // Here we are targeting the fuel value type to print the key "Gas" but becuase the function above deleted the fuel value undefined will be printed.
 }
